8f9d5a3ab341077024f4944af36d13822b178864
avinashkvar <k3076183@gmail.com>
2023-01-14 15:18:00 +0530

    new change

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 require('dotenv').config();
-const username = encodeURIComponent(process.env.USER_NAME);
-const password = encodeURIComponent(process.env.PASSWORD);
-async function connect(){
-    return new Promise((resolve,reject)=>{
-        mongoose.connect(
-				`mongodb+srv://${username}:${password}@cluster0.7aune5i.mongodb.net/newproject`,
-				(err) => {
-					if (err) {
-						return reject(err);
-					}
-					return resolve();
-				},
-			);
-    })
+const url = process.env.URL;
+
+async function connect() {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(url, (err) => {
+			if (err) {
+				return reject(err);
+			}
+			return resolve();
+		});
+	});
 }
 
-module.exports = {connect}
+module.exports = { connect };
